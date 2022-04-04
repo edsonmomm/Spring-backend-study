b7caaefaa3b09682f6f91a26ce3440f4bba31bea
Nikki Falco <nikki.falco@ninjarmm.com>
2022-04-04 13:19:43 -0400

    Initial commit

---
+++ src/main/java/com/ninjaone/backendinterviewproject/BackendInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ninjaone.backendinterviewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInterviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInterviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ninjaone/backendinterviewproject/BackendInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjaone.backendinterviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
