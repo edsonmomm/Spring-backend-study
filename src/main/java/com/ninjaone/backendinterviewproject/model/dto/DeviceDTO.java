99589071524c72e1448b896878790953464a32c2
edsonmomm <edson.momm@gmail.com>
2022-10-30 08:32:30 -0300

    Sum of services for DeviceDTO

--- src/main/java/com/ninjaone/backendinterviewproject/model/dto/DeviceDTO.java
+++ src/main/java/com/ninjaone/backendinterviewproject/model/dto/DeviceDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @AllArgsConstructor
@@ -12,6 +13,18 @@ import java.util.List;
 public class DeviceDTO {
     private Integer id;
     private String systemName;
+    private BigDecimal totalServiceCost;
     private DeviceTypeDTO deviceType;
     private List<ServiceCostDTO> services;
+
+    /**
+     * Return the sum of the services chosen by costumer for the device
+     */
+    public BigDecimal getTotalServiceCost() {
+        BigDecimal totalCost = BigDecimal.ZERO;
+        if (services != null && !services.isEmpty()) {
+            totalCost = services.stream().map(ServiceCostDTO::getValue).reduce(BigDecimal::add).get();
+        }
+        return totalCost;
+    }
 }
