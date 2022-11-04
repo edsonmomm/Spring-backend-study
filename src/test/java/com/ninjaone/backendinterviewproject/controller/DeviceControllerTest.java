d59be3d102c2f3bde20ecbc74d092a59c315b882
Edson Momm <edson.momm@gmail.com>
2022-11-04 17:38:59 -0300

    unit tests for device service

---
+++ src/test/java/com/ninjaone/backendinterviewproject/controller/DeviceControllerTest.java
@@ -0,0 +1,66 @@
+package com.ninjaone.backendinterviewproject.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ninjaone.backendinterviewproject.BackendInterviewProjectApplication;
+import com.ninjaone.backendinterviewproject.model.Device;
+import com.ninjaone.backendinterviewproject.model.dto.DeviceDTO;
+import com.ninjaone.backendinterviewproject.service.DeviceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {BackendInterviewProjectApplication.class})
+@WebMvcTest(DeviceController.class)
+@AutoConfigureMockMvc
+@AutoConfigureDataJpa
+@Transactional
+public class DeviceControllerTest {
+    public static final Integer deviceId = 991;
+    public static final String systemName = "Test device system";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private DeviceService deviceService;
+
+    private DeviceDTO deviceDTO;
+    private Device deviceEntity;
+
+    @BeforeEach
+    void setup(){
+        deviceDTO = new DeviceDTO();
+        deviceDTO.setId(deviceId);
+        deviceDTO.setSystemName(systemName);
+    }
+
+    @Test
+    void getDeviceDataById() throws Exception {
+        when(deviceService.getDeviceById(deviceId)).thenReturn(deviceDTO);
+
+        mockMvc.perform(get("/device/" + deviceId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(objectMapper.writeValueAsString(deviceDTO)));
+
+    }
+}
--- src/test/java/com/ninjaone/backendinterviewproject/service/DeviceServiceTest.java
+++ src/test/java/com/ninjaone/backendinterviewproject/service/DeviceServiceTest.java
@@ -1,34 +1,44 @@
 package com.ninjaone.backendinterviewproject.service;
 
 import com.ninjaone.backendinterviewproject.database.*;
+import com.ninjaone.backendinterviewproject.exception.DeviceNotFoundException;
 import com.ninjaone.backendinterviewproject.model.Customer;
 import com.ninjaone.backendinterviewproject.model.Device;
-import com.ninjaone.backendinterviewproject.model.dto.CustomerDTO;
-import com.ninjaone.backendinterviewproject.model.dto.DeviceDTO;
+import com.ninjaone.backendinterviewproject.model.DeviceServiceCost;
+import com.ninjaone.backendinterviewproject.model.DeviceType;
+import com.ninjaone.backendinterviewproject.model.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 @Transactional
 public class DeviceServiceTest {
-    //public static final Integer customerId = 1111;
     public static final Integer deviceId = 991;
-    public static final String systemName = "Test device system";
+    public static final String systemName = "Test for device system";
     public static final Integer deviceTypeId = 1;
+    public static final String deviceTypeName = "Test for Device Type";
+    public static final Integer customerId = 1111;
+    public static final String customerName = "Customer Name For Test";
+    public static final String customerNationalId = "0101010101";
+
 
     @Mock
     private ModelMapper modelMapper;
@@ -36,25 +46,38 @@ public class DeviceServiceTest {
     @Mock
     private DeviceRepository deviceRepository;
 
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private DeviceTypeRepository deviceTypeRepository;
+    @Mock
+    private DeviceServiceCostRepository deviceServiceCostRepository;
+
     @InjectMocks
     private DeviceService deviceService;
 
     private DeviceDTO deviceDTO;
     private Device deviceEntity;
-    //private CustomerDTO customerDTO;
-    //private Customer customerEntity;
+    private Customer customerEntity;
+    private DeviceType deviceTypeEntity;
+    private DeviceTypeDTO deviceTypeDTO;
 
     @BeforeEach
     void setup(){
+        customerEntity = new Customer(customerId, customerName, customerNationalId, new ArrayList<>());
+        deviceTypeDTO = new DeviceTypeDTO(deviceTypeId, deviceTypeName);
+        deviceTypeEntity = new DeviceType(deviceTypeId, deviceTypeName);
+
         deviceDTO = new DeviceDTO();
         deviceDTO.setId(deviceId);
         deviceDTO.setSystemName(systemName);
+        deviceDTO.setDeviceType(deviceTypeDTO);
 
         deviceEntity = new Device();
         deviceEntity.setId(deviceId);
         deviceEntity.setSystemName(systemName);
     }
-    // Test getDeviceById
+
     @Test
     void getDeviceDataById() {
         when(deviceRepository.findById(deviceId)).thenReturn(Optional.of(deviceEntity));
@@ -66,9 +89,74 @@ public class DeviceServiceTest {
         assertEquals(deviceEntity.getId(), actualEntity.getId());
         assertEquals(deviceEntity.getSystemName(), actualEntity.getSystemName());
     }
-    // Test getCostByDevice
-    // Test createDevice
-    // Test updateDevice
-    // Test deleteDevice
-    // Test getDeviceById_notfound
+
+    @Test
+    void createDevice() {
+        NewDeviceRequest newDeviceRequest = new NewDeviceRequest();
+        newDeviceRequest.setCustomerId(customerId);
+        newDeviceRequest.setSystemName(systemName);
+        newDeviceRequest.setDeviceTypeId(deviceTypeId);
+        newDeviceRequest.setDeviceServiceRequests(new ArrayList<>());
+
+        when(deviceTypeRepository.findById(deviceTypeId)).thenReturn(Optional.of(deviceTypeEntity));
+        when(customerRepository.findById(customerId)).thenReturn(Optional.of(customerEntity));
+        when(deviceRepository.findBySystemName(systemName)).thenReturn(Optional.empty());
+        when(deviceRepository.findById(deviceId)).thenReturn(Optional.of(deviceEntity));
+        when(deviceRepository.save(deviceEntity)).thenReturn(deviceEntity);
+        when(modelMapper.map(deviceEntity, DeviceDTO.class)).thenReturn(deviceDTO);
+        when(modelMapper.map(newDeviceRequest, Device.class)).thenReturn(deviceEntity);
+
+        deviceDTO = deviceService.createDevice(newDeviceRequest);
+
+        assert deviceDTO.getId() != null;
+        assertEquals(deviceDTO.getSystemName(), systemName);
+        assertEquals(deviceDTO.getDeviceType().getId(), deviceTypeId);
+        assertEquals(deviceDTO.getTotalServiceCost(), BigDecimal.ZERO);
+    }
+
+    @Test
+    void updateDevice() {
+        List<DeviceServiceCost> deviceServiceCostList = new ArrayList<>();
+        UpdateDeviceRequest updateDeviceRequest = new UpdateDeviceRequest();
+        updateDeviceRequest.setId(deviceId);
+        updateDeviceRequest.setCustomerId(customerId);
+        updateDeviceRequest.setSystemName(systemName);
+        updateDeviceRequest.setDeviceTypeId(deviceTypeId);
+        updateDeviceRequest.setDeviceServiceRequests(new ArrayList<>());
+
+        when(deviceTypeRepository.findById(deviceTypeId)).thenReturn(Optional.of(deviceTypeEntity));
+        when(customerRepository.findById(customerId)).thenReturn(Optional.of(customerEntity));
+        when(deviceRepository.findBySystemName(systemName)).thenReturn(Optional.empty());
+        when(deviceRepository.findById(deviceId)).thenReturn(Optional.of(deviceEntity));
+        when(deviceRepository.save(deviceEntity)).thenReturn(deviceEntity);
+        when(modelMapper.map(deviceEntity, DeviceDTO.class)).thenReturn(deviceDTO);
+        when(modelMapper.map(updateDeviceRequest, Device.class)).thenReturn(deviceEntity);
+        when(deviceServiceCostRepository.findByDeviceId(deviceId)).thenReturn(deviceServiceCostList);
+        doNothing().when(deviceServiceCostRepository).deleteById(0);
+
+        deviceDTO = deviceService.updateDevice(updateDeviceRequest);
+
+        assert deviceDTO.getId() != null;
+        assertEquals(deviceDTO.getSystemName(), systemName);
+        assertEquals(deviceDTO.getDeviceType().getId(), deviceTypeId);
+        assertEquals(deviceDTO.getTotalServiceCost(), BigDecimal.ZERO);
+    }
+
+    @Test
+    void deleteDevice() {
+        when(deviceRepository.findById(deviceId)).thenReturn(Optional.of(deviceEntity));
+        doNothing().when(deviceRepository).deleteById(deviceEntity.getId());
+
+        deviceService.deleteDevice(deviceId);
+        Mockito.verify(deviceRepository, times(1)).deleteById(deviceId);
+    }
+
+    @Test
+    void getDeviceById_notfound() {
+        try {
+            DeviceDTO actualEntity = deviceService.getDeviceById(deviceId);
+        } catch (DeviceNotFoundException e) {
+            assert true;
+        }
+    }
 }
